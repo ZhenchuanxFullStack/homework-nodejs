e681ab4b3e92a3b58c3593c9b4a01e55da5dad4c
Vongola <1085280247@qq.com>
2019-07-19 10:05:12 +0800

    修改测试数据为真实环境数据

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const exec = util.promisify(require('child_process').exec)
 
 const formatData = async () => {
   // cpu 信息
-  const cpuInfo = await getSysInfo('abc.txt')
-  const memInfo = await getSysInfo('abc.txt')
-  const processInfo = await getSysInfo('abc.txt')
+  const cpuInfo = await getSysInfo('/proc/cpuinfo')
+  const memInfo = await getSysInfo('/proc/meminfo')
+  const processInfo = await getProcessInfo('--sort=-%cpu')
   return {
     cpuInfo,
     memInfo,
@@ -15,12 +15,38 @@ const formatData = async () => {
   }
 }
 const getSysInfo = async (device) => {
-  const { stdout, stderr } = await exec(`cat ./${device}`);
+  const { stdout, stderr } = await exec(`cat ${device}`);
   if (stderr) return false
-  return stdout.replace(/(?!\n)[\s]+/g,'').split('\n').map(val => {
-    const [key, value] = val.split(':')
-    return {key, value}
+  const info = {}
+  stdout.replace(/(?!\n)[\s]+/g,'').split('\n').map(line => {
+    const [key, value] = line.split(':')
+    info[key] = value
   })
+  return info
+}
+
+const getProcessInfo = async (option) => {
+  // 测试就只显示10个
+  const { stdout, stderr } = await exec(`ps aux ${option}| head -n 11`);
+  if (stderr) return false
+  let label = {}
+  const infoList = []
+  stdout.split('\n').map((line, idx) => {
+    // 跳过空行
+    if (line === '') return
+
+    if (idx === 0) {
+      label = line.split(' ').filter(key => key)
+      return
+    }
+    const info = {}
+    line.split(' ').filter(key => key).map((value, index) => {
+      if (!label[index]) return
+      info[label[index]] = value
+    })
+    infoList.push(info)
+  })
+  return infoList
 }
 
 const server = http.createServer(async (req, res) => {
