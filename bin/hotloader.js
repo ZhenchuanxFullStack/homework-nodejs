a30f6254e87e85e5bc0bf3832a5fe273bca1cfea
qingkou <qingkou@163.com>
2019-08-01 23:29:21 +0800

    支持watch目录

--- bin/hotloader.js
+++ bin/hotloader.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
+const chokidar = require("chokidar");
 
 class HotLoader {
 	static getInstance() {
@@ -13,14 +14,21 @@ class HotLoader {
 
 	run(program) {
 		this.commander = program;
-		fs.watch(path.resolve(this.commander.application), (event, filename) => {
-			this.restartProcess();
-		});
+		const watcher = chokidar
+			.watch(".", { ignored: /(^|[\/\\])\../, atomic: 500 })
+			.on("change", filepath => {
+				console.log(
+					"these files has been changed:\n",
+					path.resolve(__dirname, filepath),
+					"\n"
+				);
+				this.restartProcess();
+			});
 		this.startProgress();
 	}
 
 	startProgress() {
-		this.process = spawn("node", [this.commander.application]);
+		this.process = exec(`node ${this.commander.application}`);
 		this.process.stdout.on("data", data => {
 			console.log(data.toString());
 		});
